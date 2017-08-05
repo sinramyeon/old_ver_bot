940eed70b4df6386e1a95dfc63ef60d78eb6b52c
suhyun <whtngus3232@naver.com>
2017-08-05 19:02:23 +0900

    zdnet top 10 news

---
+++ crawling/zdnet.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+# 인기시사 top 10 가져오기 
+def zdnet():
+    contents = {}
+    headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0' }
+    html = requests.get('http://www.zdnet.co.kr/news/news_list.asp?zdknum=0000&lo=z3',headers=headers).text
+    soup = BeautifulSoup(html,'html.parser')
+    for tag in soup.select('li div a'):
+        title_name = tag.select('span')[0].text.strip()
+        url = tag['href']
+        choice = {
+                'url' : url,
+                'title_name' : title_name,
+        }
+        contents[url]=choice
+    return contents
+
+print(zdnet())
