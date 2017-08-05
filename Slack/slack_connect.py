a122c4f97b5c6282b9c4c20bd7f9ecbec527997a
hero0926 <esistkalt@naver.com>
2017-08-05 17:21:22 +0900

    채널별 메시지 보내기

--- Slack/slack_connect.py
+++ Slack/slack_connect.py
@@ -14,19 +14,18 @@ def connect(token) :
 
 # 2. 슬랙에 메시지를 보냅니다.
 
-def postMsg(s, channel, text, attachments) :
+def postMsg(s, channel, text) :
     try :
 
-        obj = s.chat.post_message(
-            channel = channel,
-            text = text,
-            attachments = attachments,
-        )
-
-        print (obj.successful, obj.__dict__['body']['channel'], obj.__dict__[
-            'body']['ts'])
+       s.api_call(
+           "chat.postMessage",
+           channel = channel,
+           text = text,
+           usernmae = 'iparrot',
+           
+       )
     except KeyError :
-        print ("연결에 실패하였습니다")
+        print ("메시지 보내기에 실패하였습니다")
 
 
 # 3. 슬랙 메시지를 받아 옵니다.
@@ -51,17 +50,38 @@ def list_channels(s):
         return channels_call['channels']
     return None
 
+# 5. 채널 정보를 가져옵니다.
+def channel_info(s, channel):
+    channel_info = s.api_call("channels.info", channel=channel)
+    if channel_info:
+        return channel_info['channel']
+    return None
 
 token = 'xoxb-222751163605-d3ym2ljrLP3FU5EqLmHWUeLd'
 hack_channel = "hackaton2017"
 
 
 if __name__ == "__main__" :
+
     s = connect(token)
     channels = list_channels(s)
+
     if channels :
         print("채널 목록 : ")
         for c in channels :
-            print(c['name'] + " (" + c['id'] + ")")
+            # 채널 아이디 얻기
+            detailed_info = channel_info(s, c['id'])
+            if detailed_info :
+
+                print(c['name']+"채널의 아이디는 : "+c['id'])
+            if c['name'] == 'general' :
+                postMsg(s, c['id'], "general 에 보냅니다.")
+            if c['name'] == 'hackaton2017' :
+                postMsg(s, c['id'], "hackaton2017 에 보냅니다.")
+            if c['name'] == 'random' :
+                postMsg(s, c['id'], "random 에 보냅니다.")
     else:
         print("연결 실패....")
+
+
+
